010c69a6cbfff29af7f01c3c0a54b995141ddb0a
MarkZhuYiQi <zhuyiqi_026@163.com>
2016-07-10 22:41:49 +0800

    add tranisition effect, login logoff status

---
+++ libs/Model/adminModel.class.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/7/3
+ * Time: 17:24
+ */
+
+namespace libs\Model;
+
+
+class adminModel
+{
+    function index(){
+        return "hello World!";
+    }
+}
