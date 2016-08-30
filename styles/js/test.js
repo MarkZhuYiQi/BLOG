64f64b3c4cdf3ccd752c851b4447415ede7d4d35
MarkZhu <zhuyiqi_026@163.com>
2016-08-30 17:40:30 +0800

    add articlemodule

---
+++ styles/js/test.js
@@ -0,0 +1,25 @@
+/**
+ * Created by SZL4ZSY on 8/30/2016.
+ */
+$(document).ready(function(){
+    $("#sidebar .content").hide();
+
+    //切换不同的菜单
+    $("#sidebar .btn-danger").click(function(){
+        var btnIndex=this.parentNode.parentNode.parentNode;
+        for(var i=0;i<btnIndex.parentNode.children.length;i++){
+            if($(btnIndex).index()==i){    //如果是当前点击目标
+                $("#sidebar .content").eq(i).slideToggle();
+            }else{
+                $("#sidebar .content").eq(i).slideUp();
+            }
+        }
+    });
+    //按钮按下之后记忆锁定
+    $("#sidebar dd button").click(function(){
+        $("#sidebar dd button").removeClass("active");
+        $(this).addClass("active");
+        $("#main").load("test2.html");
+    });
+
+});
